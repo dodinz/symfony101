75ff0c123333c302d30aa02463d4e90f24a272d0
Dodinz <dindo.ballecer@gmail.com>
2018-06-29 09:37:33 +0800

    Controller Basic
    - Remove AbstractController from base
    - Change how to get parameter from route

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,25 +2,33 @@
 namespace App\Controller;
 
 use App\Service\Greeting;
+use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class BlogController extends AbstractController
+class BlogController
 {
     private $greeting;
-    
-    public function __construct( Greeting $greeting) {
+    /**
+     * @var \Twig_Environment
+     */
+    private $template;
+
+    public function __construct( Greeting $greeting, \Twig_Environment $template ){
         
         $this->greeting = $greeting;
+        $this->template = $template;
     }
     /**
-     * @Route("/", name="blog_index")
+     * @Route("/{name}", name="blog_index")
      */
-    public function index( Request $request ) {
-        return $this->render( 'base.html.twig' ,
-                ['message' => $this->greeting->greet( $request->get('name')) 
+    public function index( $name ) {
+        $html = $this->template->render( 'base.html.twig' ,
+                ['message' => $this->greeting->greet( $name )
         ]);
+
+        return new Response( $html );
     }
     
 }
